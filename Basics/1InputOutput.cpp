eec2e42e44467a9e3086e3cc040f539af6fb8203
Priya Anand <192531821+quantum-trace@users.noreply.github.com>
2026-09-19 14:35:15 +0530

    Initial commit

---
+++ Basics/1InputOutput.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+//#include<iostream>
+using namespace std;
+
+int main()
+{
+//Input
+int x;
+cout<<"Value :" ;
+cin>>x;
+
+//Output
+
+cout<<x<<endl;
+cout<<x<<"\n";
+
+//"\n" and endl are used with only cout not cin
+return 0;
+}
